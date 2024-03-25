c020851d971d49ef00fb3c2271c588d058cc4268
Mateus Oliveira <44128449865@escola928.edu>
2024-03-25 16:30:41 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,47 +1,82 @@
 document.addEventListener('DOMContentLoaded', () => {
   const cocktailList = document.getElementById('cocktail-list');
   const searchForm = document.querySelector('form');
+  const randomDrinkBtn = document.getElementById('random-drink-btn');
 
   function searchCocktails(query) {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
-      .then(response => response.json())
-      .then(data => {
-        cocktailList.innerHTML = ''; // Limpar a lista antes de adicionar novos itens
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+          .then(response => response.json())
+          .then(data => {
+              cocktailList.innerHTML = '';
 
-        const cocktails = data.drinks;
+              const cocktails = data.drinks;
 
-        cocktails.forEach(cocktail => {
-          const cocktailItem = document.createElement('div');
-          cocktailItem.classList.add('cocktail-item');
+              cocktails.forEach(cocktail => {
+                  const cocktailItem = createCocktailItem(cocktail);
+                  cocktailList.appendChild(cocktailItem);
+              });
+          })
+          .catch(error => {
+              console.error('Erro ao carregar os coquetéis:', error);
+          });
+  }
 
-          const cocktailName = document.createElement('h2');
-          cocktailName.textContent = cocktail.strDrink;
+  function createCocktailItem(cocktail) {
+      const cocktailItem = document.createElement('div');
+      cocktailItem.classList.add('col-md-4', 'col-lg-3', 'mb-4');
 
-          const cocktailImage = document.createElement('img');
-          cocktailImage.src = cocktail.strDrinkThumb;
-          cocktailImage.alt = cocktail.strDrink;
+      const cocktailCard = document.createElement('div');
+      cocktailCard.classList.add('card');
 
-          const cocktailInstructions = document.createElement('p');
-          cocktailInstructions.textContent = cocktail.strInstructions;
+      const cocktailImage = document.createElement('img');
+      cocktailImage.src = cocktail.strDrinkThumb;
+      cocktailImage.classList.add('card-img-top');
+      cocktailImage.alt = cocktail.strDrink;
 
-          cocktailItem.appendChild(cocktailName);
-          cocktailItem.appendChild(cocktailImage);
-          cocktailItem.appendChild(cocktailInstructions);
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
 
-          cocktailList.appendChild(cocktailItem);
-        });
-      })
-      .catch(error => {
-        console.error('Erro ao carregar os coquetéis:', error);
-      });
+      const cocktailName = document.createElement('h5');
+      cocktailName.classList.add('card-title');
+      cocktailName.textContent = cocktail.strDrink;
+
+      const cocktailInstructions = document.createElement('p');
+      cocktailInstructions.classList.add('card-text');
+      cocktailInstructions.textContent = cocktail.strInstructions;
+
+      cardBody.appendChild(cocktailName);
+      cardBody.appendChild(cocktailInstructions);
+
+      cocktailCard.appendChild(cocktailImage);
+      cocktailCard.appendChild(cardBody);
+
+      cocktailItem.appendChild(cocktailCard);
+
+      return cocktailItem;
+  }
+
+  function getRandomDrink() {
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+          .then(response => response.json())
+          .then(data => {
+              cocktailList.innerHTML = ''; 
+              const randomDrink = data.drinks[0];
+              const randomDrinkItem = createCocktailItem(randomDrink);
+              cocktailList.appendChild(randomDrinkItem);
+          })
+          .catch(error => {
+              console.error('Erro ao carregar o drink aleatório:', error);
+          });
   }
 
   searchForm.addEventListener('submit', event => {
-    event.preventDefault(); // Evitar o comportamento padrão do formulário
+      event.preventDefault(); 
 
-    const searchTerm = searchForm.querySelector('input').value.trim();
-    if (searchTerm !== '') {
-      searchCocktails(searchTerm);
-    }
+      const searchTerm = searchForm.querySelector('input').value.trim();
+      if (searchTerm !== '') {
+          searchCocktails(searchTerm);
+      }
   });
+
+  randomDrinkBtn.addEventListener('click', getRandomDrink);
 });
